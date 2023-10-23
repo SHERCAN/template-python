dd9238020594cab4d7d4d7428c457ede9500bc9c
SHERCAN <sidereoyess@gmail.com>
2023-10-22 20:58:13 -0500

    Introduccion

--- static/js/configuracion.js
+++ static/js/configuracion.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const agregarFilaBtn = document.getElementById("agregarFila");
+  const agregarOperarioBtn = document.getElementById("agregarOperario");
   const guardarBtn = document.getElementById("guardar");
+  const tablaAdministradores = document.querySelectorAll(
+    "#tablaAdministradores table"
+  );
+  const tablaServidorPeso = document.querySelectorAll(
+    "#tablaServidorPeso table"
+  );
   const formulario = document
     .getElementById("formulario")
     .getElementsByTagName("tbody")[0];
-
+  const formularioOperario = document
+    .getElementById("formularioOperario")
+    .getElementsByTagName("tbody")[0];
   agregarFilaBtn.addEventListener("click", function () {
     const newRow = document.createElement("tr");
     newRow.innerHTML = `
@@ -18,9 +27,20 @@ document.addEventListener("DOMContentLoaded", function () {
     formulario.appendChild(newRow);
     addDeleteRowListener(newRow); // Agrega el listener al nuevo botón
   });
+  agregarOperarioBtn.addEventListener("click", function () {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+    <td>      <input type="text" name="titulo" class="form-control" /></td>
+        <td><input type="text" class="form-control" /></td>
+    <td>      <input type="password" class="form-control" />    </td>
+    <td>      <button class="btn btn-danger delete-row">-</button>    </td>
+        `;
+    formularioOperario.appendChild(newRow);
+    addDeleteRowListener(newRow); // Agrega el listener al nuevo botón
+  });
 
   guardarBtn.addEventListener("click", function () {
-    const data = [];
+    const datos = [];
     const rows = formulario.getElementsByTagName("tr");
 
     for (let i = 0; i < rows.length; i++) {
@@ -31,13 +51,96 @@ document.addEventListener("DOMContentLoaded", function () {
           Obligatorio: cols[1].querySelector(".form-check-input").checked,
           Autocompletado: cols[2].querySelector(".form-check-input").checked,
         };
-        data.push(item);
+        datos.push(item);
       }
     }
 
-    // Convertir a JSON
-    const jsonData = JSON.stringify(data, null, 2);
-    alert(jsonData);
+    const datosOperarios = [];
+    const rowsOperarios = formularioOperario.getElementsByTagName("tr");
+
+    for (let i = 0; i < rowsOperarios.length; i++) {
+      const cols = rowsOperarios[i].getElementsByTagName("td");
+      if (cols.length === 4) {
+        const item = {
+          Operador: cols[0].querySelector(".form-control").value,
+          Usuario: cols[1].querySelector(".form-control").value,
+          Contraseña: cols[2].querySelector(".form-control").value,
+        };
+        datosOperarios.push(item);
+      }
+    }
+
+    var formDataUsuario = datos;
+    var formOperarios = datosOperarios;
+    var tipoConexion = document.getElementById("tipoConexion").value;
+    var formDataConexion = {};
+
+    if (tipoConexion === "serial") {
+      formDataConexion = {
+        puertoSerial: document.getElementById("puertoSerial").value,
+        baudiosSerial: document.getElementById("baudiosSerial").value,
+        paridadSerial: document.getElementById("paridadSerial").value,
+      };
+    } else if (tipoConexion === "tcpip") {
+      formDataConexion = {
+        hostTCP: document.getElementById("hostTCP").value,
+        puertoTCP: document.getElementById("puertoTCP").value,
+      };
+    }
+    var dataAdministradores = [];
+    tablaAdministradores.forEach((table, index) => {
+      // Obtén todas las filas de la tabla
+      const rows = table.querySelectorAll("tbody tr");
+      console.log(rows);
+      // Crea un objeto para almacenar los datos de un administrador
+      const adminData = {};
+
+      // Recorre las filas y extrae los valores de los campos
+      rows.forEach((row) => {
+        const cells = row.querySelectorAll("td");
+        const fieldName = cells[0].textContent.trim();
+        const fieldValue = cells[1].querySelector("input").value;
+
+        // Asigna el valor al objeto del administrador
+        adminData[fieldName] = fieldValue;
+      });
+
+      // Agrega el objeto del administrador al arreglo de datos
+      dataAdministradores.push(adminData);
+    });
+    var dataServidorPeso = [];
+    tablaServidorPeso.forEach((table, index) => {
+      // Obtén todas las filas de la tabla
+      const rows = table.querySelectorAll("tbody tr");
+      console.log(rows);
+      // Crea un objeto para almacenar los datos de un administrador
+      const adminData = {};
+
+      // Recorre las filas y extrae los valores de los campos
+      rows.forEach((row) => {
+        const cells = row.querySelectorAll("td");
+        const fieldName = cells[0].textContent.trim();
+        const fieldValue = cells[1].querySelector("input").value;
+
+        // Asigna el valor al objeto del administrador
+        adminData[fieldName] = fieldValue;
+      });
+
+      // Agrega el objeto del administrador al arreglo de datos
+      dataServidorPeso.push(adminData);
+    });
+    var data = {
+      datosUsuario: formDataUsuario,
+      configuracionConexion: {
+        tipoConexion: tipoConexion,
+        datos: formDataConexion,
+      },
+      datosOperarios: formOperarios,
+      datosAdministradores: dataAdministradores,
+      datosServidorPeso: dataServidorPeso,
+    };
+
+    var jsonData = JSON.stringify(data);
     console.log(jsonData);
   });
 
